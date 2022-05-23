870a092a1f2cc31b171b7ccbbcfee193ede7a8a2
MookieUm <tony7320@gmail.com>
2022-05-23 23:13:11 +0900

    add non-blocking code with redirect

--- app.js
+++ app.js
@@ -23,10 +23,12 @@ const server = http.createServer((req, res) => {
         return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-            res.statusCode = 302;
-            res.setHeader('Location', '/');
-            return res.end();
+            fs.writeFile('message.txt', message, (arr) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+            
         });
     }
     // console.log(req.url, req.method, req.headers);
