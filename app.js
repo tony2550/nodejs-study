0d16776e85c29b42931a1a4bacbeec5c20083ff9
김도현 <mookieum1991@gmail.com>
2022-07-07 17:42:25 +0900

    add add-product form and add body-parser

--- app.js
+++ app.js
@@ -1,23 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/', (req, res, next) => {
     // default route
-    console.log('prepare page');
+    console.log('this always runs!');
     next();
 });
 
-app.use('/list', (req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     // '/list'
-    console.log('list page');
-    res.send('<h1>It is List Page</h1>');
+    console.log('add-product page');
+    res.send(
+        '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    );
 });
-
+// 인수 생략가능 app.use('/product', (req, res, next))
+app.use('/product', (req, res) => {
+    console.log(req.body);
+    res.redirect('/'); // response 리다이렉트ddd
+});
+//
 app.use('/', (req, res, next) => {
-    // default route
+    // default routedd
     console.log('Default page');
-    res.send('<h1>Thanks to ExpressJS!</h1>');
+    res.send('<h1>Thanks to ExpressJSd!</h1>');
 });
 
 app.listen(8090, () => {
